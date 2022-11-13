3e32bffd96ced671482263161c558823121971e3
martin <martin114@foxmail.com>
2022-11-13 09:32:33 +0800

    :zap: fix(erd): 首页增加统计项
    :zap: fix(erd): 首页增加统计项

---
+++ src/main/java/com/java2e/martin/extension/ncnb/dto/ProjectBaseDto.java
@@ -0,0 +1,20 @@
+package com.java2e.martin.extension.ncnb.dto;
+
+import lombok.Data;
+
+import java.util.Map;
+
+/**
+ * @author: liangcan
+ * @version: 1.0
+ * @date: 2022/11/11 13:34
+ * @describtion: ProjectDto
+ */
+@Data
+public class ProjectBaseDto {
+    private String id;
+    private String projectName;
+    private String tags;
+    private String description;
+
+}
